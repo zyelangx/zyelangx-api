a58d0f1fafb34c7f012e0eaa5851c1b3c207c4e6
ZyeLangX <131007575+zyelangx@users.noreply.github.com>
2023-04-18 12:03:45 +0700

    Update menu.js

--- public/menu.js
+++ public/menu.js
@@ -20,222 +20,6 @@
     </ul>
 </li> */}
 
-const menu = [
-    {
-        icon: "",
-        name: "asupan",
-        items: [
-            {
-                name: "bocil",
-                url: "/api/asupan/Bocil"
-            },
-            {
-                name: "geayubi",
-                url: "/api/asupan/geayubi"
-            },
-            {
-                name: "gheayubi",
-                url: "/api/asupan/gheayubi"
-            },
-            {
-                name: "hijaber",
-                url: "/api/asupan/hijaber"
-            },
-            {
-                name: "santuy",
-                url: "/api/asupan/santuy"
-            },
-            {
-                name: "rikagusriani",
-                url: "/api/asupan/rikagusriani"
-            },
-            {
-                name: "random",
-                url: "/api/asupan/random"
-            }
-        ]
-    },
-    {
-        icon: "",
-        name: "image",
-        items: [
-            {
-                name: "cogan",
-                url: "/api/image/cogan"
-            },
-            {
-                name: "cecan",
-                url: "/api/image/cecan"
-            },
-            {
-                name: "aesthetic",
-                url: "/api/image/aesthetic"
-            },
-            {
-                name: "anjing",
-                url: "/api/image/anjing"
-            },
-            {
-                name: "antiwork",
-                url: "/api/image/antiwork"
-            },
-            {
-                name: "blackpink",
-                url: "/api/image/blackpink"
-            },
-            {
-                name: "boneka",
-                url: "/api/image/boneka"
-            },
-            {
-                name: "cecan",
-                url: "/api/image/cecan"
-            },
-            {
-                name: "cosplay",
-                url: "/api/image/cosplay"
-            },
-            {
-                name: "dadu",
-                url: "/api/image/dadu"
-            },
-            {
-                name: "justina",
-                url: "/api/image/justina"
-            },
-            {
-                name: "kayes",
-                url: "/api/image/kayes"
-            },
-            {
-                name: "kpop",
-                url: "/api/image/kpop"
-            },
-            {
-                name: "kucing",
-                url: "/api/image/kucing"
-            },
-            {
-                name: "mobil",
-                url: "/api/image/mobil"
-            },
-            {
-                name: "notnot",
-                url: "/api/image/notnot"
-            },
-            {
-                name: "profil",
-                url: "/api/image/profil"
-            },
-            {
-                name: "rose",
-                url: "/api/image/rose"
-            },
-            {
-                name: "ryujin",
-                url: "/api/image/ryujin"
-            },
-            {
-                name: "wallhp",
-                url: "/api/image/wallhp"
-            },
-            {
-                name: "wallml",
-                url: "/api/image/wallml"
-            }
-        ]
-    },
-    {
-        icon: "",
-        name: "sfw",
-        items: [
-            {
-                name: "milf",
-                url: "/api/sfw/milf"
-            },
-            {
-                name: "loli",
-                url: "/api/sfw/loli"
-            },
-            {
-                name: "neko",
-                url: "/api/sfw/neko"
-            },
-            {
-                name: "hubsu",
-                url: "/api/sfw/hubsu"
-            },
-            {
-                name: "shota",
-                url: "/api/sfw/shota"
-            },
-            {
-                name: "waifu",
-                url: "/api/sfw/waifu"
-            }
-        ]
-    },
-    {
-        icon: "",
-        name: "islamic",
-        items: [
-            {
-                name: "al-quran",
-                url: "/api/islamic/al-quran"
-            },
-            {
-                name: "al-quran",
-                desc: "params ayat 1-114",
-                url: "/api/islamic/al-quran/1"
-            },
-            {
-                name: "asmaul husna",
-                desc: "params ayat 1-99",
-                url: "/api/islamic/asmaul_husna"
-            },
-            {
-                name: "kisah nabi",
-                desc: "params nama nabi adam-muhammad",
-                url: "/api/islamic/kisahnabi"
-            }
-        ]
-    },
-    {
-        icon: "",
-        name: "edukasi",
-        items: [
-            {
-                name: "covidworld",
-                url: "/api/edukasi/covidworld"
-            },
-            {
-                name: "infogempa",
-                url: "/api/edukasi/infogempa/"
-            }
-        ]
-    },
-    {
-        icon: "",
-        name: "tools",
-        items: [
-            {
-                name: "Ip",
-                url: "/api/tools/ip"
-            },
-            {
-                name: "Ip Lookup",
-                desc: "params ip ex: 8.8.8.8",
-                url: "/api/tools/ip/8.8.8.8"
-            },
-            {
-                name: "stalk ff",
-                desc: "query id ex: 2637837475",
-                url: "/api/tools/stalkff?id=2637837475"
-            }
-        ]
-    }
-]
-
 const menus = {
     asupan: [
         {
@@ -430,6 +214,16 @@ const menus = {
             desc: "query id ex: 2637837475",
             url: "/api/tools/stalkff?id=2637837475"
         }
+    ],
+    downloader:[
+        {
+            name: "Youtube Mp4",
+            url: "/api/downloader/ytmp4?url=https://youtu.be/Nq5rzeJ5Ab4"
+        },
+        {
+            name: "Youtube Mp3",
+            url: "/api/downloader/ytmp3?url=https://youtu.be/Nq5rzeJ5Ab4"
+        }
     ]
 }
 
@@ -477,6 +271,14 @@ function menuss() {
     for (var e of menus.tools){
         // Tools
         tools.innerHTML += `<li class="">
+        <a class="sidenav-item-link" href="${e.url.trim()}">
+          <span class="nav-text">${e.name.trim()}</span>
+        </a>
+      </li>`
+    }
+    for (var e of menus.downloader){
+        // Downloader
+        downloader.innerHTML += `<li class="">
         <a class="sidenav-item-link" href="${e.url.trim()}">
           <span class="nav-text">${e.name.trim()}</span>
         </a>
